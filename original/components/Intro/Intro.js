0279ea8d8e5c14a5fc2d3c49dd319a98089a7cd0
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-06 09:25:22 -0500

    add outer ring and bg

--- original/components/Intro/Intro.js
+++ original/components/Intro/Intro.js
@@ -12,6 +12,9 @@ import mustache from './mustache.png'
 import goatee from './goatee.png'
 import faceOutline from './face-outline.png'
 import hatOutline from './hat-outline.png'
+import innerRing from './inner-ring.png'
+import outerRing from './outer-ring.png'
+import mainBg from './main-bg.png'
 
 
 const Intro = () => (
@@ -23,19 +26,28 @@ const Intro = () => (
       <Parallax translateY={[50, -50]} className={style.glasses}>
         <img src={glasses} alt="glasses" id="glasses" style={{maxWidth: 300}} />
       </Parallax>
-      <Parallax scale={[-3, 5]} translateY={[-45, 45]} className={style.glasses}>
+      <Parallax scale={[-1.8, 3.8]} translateY={[-65, 65]} className={style.glasses}>
         <img src={mustache} alt="mustache" id="mustache" style={{maxWidth: 300}} />
       </Parallax>
       <Parallax scaleY={[-1, 3]} translateY={[50, -50]} className={style.glasses}>
         <img src={goatee} alt="glasses" id="glasses" style={{maxWidth: 300}} />
       </Parallax>
-      <Parallax scale={[-5, 7]} translateY={[140, -140]} className={style.glasses}>
+      <Parallax scale={[-1.8, 3.8]} translateY={[140, -140]} className={style.glasses}>
         <img src={faceOutline} alt="glasses" id="glasses" style={{maxWidth: 300}} />
       </Parallax>
-      <Parallax scale={[-20, 22]} translateY={[160, -160]} className={style.glasses}>
+      <Parallax scale={[0, 2]} translateY={[220, -220]} className={style.glasses}>
         <img src={hatOutline} alt="glasses" id="glasses" style={{maxWidth: 300}} />
       </Parallax>
-      <Svg className={style.hemispheres} svg={hemispheres} />
+      <Parallax translateY={[0, 0]} className={style.innerRing}>
+        <img src={innerRing} alt="glasses" id="glasses" style={{maxWidth: 300}} />
+      </Parallax>
+      <Parallax translateY={[90, -90]} scale={[-0.5, 2.5]} className={style.innerRing}>
+        <img src={outerRing} alt="glasses" id="glasses" style={{maxWidth: 300}} />
+      </Parallax>
+      <Parallax translateY={[-15, 15]} className={style.mainBg}>
+        <img src={mainBg} alt="glasses" id="glasses" style={{maxWidth: 300}} />
+      </Parallax>
+      {/* <Svg className={style.hemispheres} svg={hemispheres} /> */}
     </div>
     <p className={style.scroll}>¿goingdown?</p>
   </div>
