19dd76a881468782ab9dacfbb657e9f6efa6db1c
JohnnyMohawk <johnnelsonalden@gmail.com>
2022-04-08 23:37:35 -0500

    add title section and good stuff parallax

--- original/components/Marquee/Marquee.js
+++ original/components/Marquee/Marquee.js
@@ -5,12 +5,27 @@ import { Parallax } from "react-scroll-parallax";
 import boxBg from "bundle-text:./box-bg.svg";
 import boxOutline from "bundle-text:./box-outline.svg";
 import divider from "bundle-text:../shared/divider.svg";
+import divider1 from "bundle-text:../shared/divider1.svg";
+import divider2 from "bundle-text:../shared/divider2.svg";
+
+import outerRing from './images/outer-ring.png'
+import outerWhtRing from './images/Outer-white-ring.png'
+import innerRing from './images/inner-ring.png'
+import innerWhtRing from './images/Inner-white-ring.png'
+import pinkDot from './images/Inner-dot-mag.png'
 
 const Marquee = () => (
   <div className={style.root}>
     <Svg svg={divider} className={style.divider} />
+    <Parallax className={style.dividerParallax} translateY={[-65, -32]}>
+      <Svg svg={divider1} className={style.divider} />
+    </Parallax>
+    <Parallax className={style.dividerParallax} translateY={[-65.12, -45]}>
+      <Svg svg={divider2} className={style.divider} />
+    </Parallax>
     <div className={style.container}>
-      <Svg svg={boxBg} className={style.boxBg} />
+      {/* <Svg svg={boxBg} className={style.boxBg} /> */}
+      <img src={outerWhtRing} alt="faceBg" id="faceBg" style={{width: 420, marginTop: -50, marginLeft: 30, opacity: 0.7}} />
       <Parallax
         className={style.text}
         translateY={[-70, 70]}
@@ -18,8 +33,22 @@ const Marquee = () => (
       >
         <span className="h1">¿WHATAREYOULEFTWITH?</span>
       </Parallax>
-      <Parallax className={style.boxOutline} translateY={[-35, 35]}>
-        <Svg svg={boxOutline} />
+      <Parallax className={style.boxOutline} translateY={[75, -35]}>
+        {/* <Svg svg={boxOutline} /> */}
+        <img src={outerRing} alt="faceBg" id="faceBg" style={{width: 500, marginTop: -290, marginLeft: 25, opacity: 0.7}} />
+      </Parallax>
+      <Parallax className={style.boxOutline} translateY={[195, -155]}>
+        {/* <Svg svg={boxOutline} /> */}
+        <img src={innerRing} alt="faceBg" id="boxBg" style={{width: 335, marginTop: -500, marginLeft: 107, opacity: 0.7}} />
+      </Parallax>
+      <Parallax className={style.boxOutline} translateY={[95, -20]}>
+        {/* <Svg svg={boxOutline} /> */}
+        <img src={innerWhtRing} alt="faceBg" id="boxBg" style={{width: 248, marginTop: -230, marginLeft: 150, opacity: 0.7}} />
+      </Parallax>
+      <Parallax className={style.gradient} translateY={[81, -20]}>
+        {/* <Svg svg={boxOutline} /> */}
+        {/* <img src={pinkDot} alt="faceBg" id="boxBg" style={{width: 168, marginTop: -500, marginLeft: 190}} /> */}
+        <div className={style.outerCircle}style={{width: 167, marginTop: -294, marginLeft: 158}}></div>
       </Parallax>
     </div>
   </div>
--- original/components/TriangleGrid/TriangleGrid.js
+++ original/components/TriangleGrid/TriangleGrid.js
@@ -6,12 +6,30 @@ import gridPurple from "bundle-text:./grid-purple.svg";
 import gridWhite from "bundle-text:./grid-white.svg";
 import angleTop from "bundle-text:../shared/angle-dark-top.svg";
 
+import saveGood from './images/save-good.png'
+import saveGoodFlip from './images/save-good-flip.png'
+
+
 const TriangleGrid = () => (
   <main className={style.root}>
     <Svg svg={angleTop} className={style.angleTop} />
     <article className={style.copy}>
-      <h1 className={style.headline}>React Scroll Parallax</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 className={style.headline}>John Nelson-Alden</h1>
+      <h3 className={style.headline}>self-portrait in code, 2022</h3>
       <p>
+        HTML, CSS, React, React Scroll Parallax, Procreate.
+      </p>
+      <p>
+        I am an artist and software engineer from in Chicago, Illinois, USA. My art looks at dark or heavy subjects with bright colors and a light heart.
+      </p>
+      <p>
+        This piece is an exploration of using front-end developer tools as an artistic medium. It is also an exercise in shameless self-promotion. To see more of my work, click the button below. 
+      </p>
+      {/* <p>
         React hooks and components to create parallax scroll effects for
         banners, images or any other DOM elements.
       </p>
@@ -31,31 +49,21 @@ const TriangleGrid = () => (
       <p>
         <code>yarn add react-scroll-parallax</code>
         <code>npm i react-scroll-parallax</code>
-      </p>
+      </p> */}
       <p>
-        <a className="btn" href="https://react-scroll-parallax.damnthat.tv/">
-          Documentation
-        </a>
-        <a
-          className="btn"
-          href="https://github.com/jscottsmith/react-scroll-parallax"
-        >
-          GitHub
-        </a>
-        <a
-          className="btn"
-          href="https://www.npmjs.com/package/react-scroll-parallax"
-        >
-          View on NPM
+        <a className="btn" href="https://www.johnnelsonalden.com/">
+          Portfolio
         </a>
       </p>
     </article>
     <div className={style.container}>
       <Parallax translateY={[-25, 25]}>
-        <Svg svg={gridPurple} className={style.trianglesPurple} />
+        {/* <Svg svg={gridPurple} className={style.trianglesPurple} /> */}
+        <img src={saveGoodFlip} alt="faceBg" id="faceBg" style={{width: 275, marginTop: 0, marginBottom: 0, marginLeft: 0, opacity: 0.7}} />
       </Parallax>
-      <Parallax translateX={[-13, 13]} translateY={[-50, 50]}>
-        <Svg svg={gridWhite} />
+      <Parallax translateX={[-13, 13]} translateY={[-130, 50]}>
+        {/* <Svg svg={gridWhite} /> */}
+        <img src={saveGood} alt="faceBg" id="faceBg" style={{width: 275, marginTop: -1700, marginBottom: 0, marginLeft: 20, opacity: 0.7}} />
       </Parallax>
     </div>
   </main>
